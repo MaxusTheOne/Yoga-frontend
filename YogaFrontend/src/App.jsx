94306847e4de17775f5aaf5ef96fc2e55faa3413
TheDanishMexican <danieljchristjansen@gmail.com>
2023-11-21 12:06:22 +0100

    first commit

--- YogaFrontend/src/App.jsx
+++ YogaFrontend/src/App.jsx
@@ -1,10 +1,12 @@
 import Homepage from "./Homepage.jsx";
 import { Route, Routes } from "react-router-dom";
 import "./homepage.css";
+import Eventpage from "./components/Eventpage.jsx";
 
 function App() {
   return (
     <>
+    <Eventpage/>
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="*" element={<Homepage />} />
---
+++ YogaFrontend/src/components/Eventpage.jsx
@@ -0,0 +1,7 @@
+export default function Eventpage() {
+    return (
+        <div>
+            <h1>yo mama</h1>
+        </div>
+    )
+}
