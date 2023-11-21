e53bf387e1f49f9e4a22672d5a365a18c46a6a34
TheDanishMexican <danieljchristjansen@gmail.com>
2023-11-21 13:26:46 +0100

    started the calendar component

--- YogaFrontend/src/components/Eventpage/Eventpage.jsx
+++ YogaFrontend/src/components/Eventpage/Eventpage.jsx
@@ -1,11 +1,13 @@
 import Header from '../Header'
+import EventCalendar from './EventCalendar'
 
 export default function Eventpage() {
     return (
         <>
             <Header />
             <div className="event-calendar-container">
-                <h1>yo mama</h1>
+                <h1>Upcoming events</h1>
+                <EventCalendar />
             </div>
         </>
     )
