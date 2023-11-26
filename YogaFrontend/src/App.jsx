d651d3814a082874405e70ad0e83a1e9b328efaf
fili0727 <123998888+fili0727@users.noreply.github.com>
2023-11-26 14:40:23 +0100

    privatesesh site done

--- YogaFrontend/src/App.jsx
+++ YogaFrontend/src/App.jsx
@@ -2,6 +2,7 @@ import Eventpage from './components/Eventpage/Eventpage.jsx'
 import Homepage from './components/Homepage.jsx'
 import FaceYoga from './components/Homepage/FaceYoga.jsx'
 import Online from './components/Homepage/Online.jsx'
+import PrivateSessions from './components/Homepage/PrivateSessions.jsx'
 import { Route, Routes } from 'react-router-dom'
 // import "./homepage.css";
 
@@ -14,6 +15,7 @@ function App() {
                 <Route path="events" element={<Eventpage />} />
                 <Route path="faceyoga" element={<FaceYoga />} />
                 <Route path="online" element={<Online />} />
+                <Route path="privatesessions" element={<PrivateSessions />} />
             </Routes>
         </>
     )
