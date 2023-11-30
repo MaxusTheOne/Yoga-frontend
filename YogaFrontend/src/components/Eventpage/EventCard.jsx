3087fc22b8d68c9a354766e24ea3e5d324716ff1
TheDanishMexican <danieljchristjansen@gmail.com>
2023-11-30 11:41:07 +0100

    added dialog to event

--- YogaFrontend/src/components/Eventpage/EventCard.jsx
+++ YogaFrontend/src/components/Eventpage/EventCard.jsx
@@ -1,10 +1,31 @@
-export default function EventCard({ title, description }) {
+import { useState } from 'react'
+import EventDialog from './EventDialog'
+
+export default function EventCard({ matchingEvent }) {
     EventCard.propTypes
+
+    const [dialogStatus, setDialogStatus] = useState(false)
+
+    function openEventDialog() {
+        setDialogStatus(true)
+    }
+
+    function closeEventDialog() {
+        setDialogStatus(false)
+    }
+
     return (
-        <div className="eventCard-container">
-            <h3>{title}</h3>
-            <p>{description}</p>
-            <button>Sign up</button>
+        <div className="eventCard">
+            <h3 onClick={openEventDialog} className="eventCard-title">
+                {matchingEvent.title}
+            </h3>
+            <div className="calendarCard-button-container"></div>
+            {dialogStatus && (
+                <EventDialog
+                    matchingEvent={matchingEvent}
+                    closeEventDialog={closeEventDialog}
+                />
+            )}
         </div>
     )
 }
