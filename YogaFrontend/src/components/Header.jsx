1d0bb10f94bd028f8a2efe10eac9f4f504eac2dc
fili0727 <123998888+fili0727@users.noreply.github.com>
2023-11-30 15:38:58 +0100

    button css

--- YogaFrontend/src/components/Header.jsx
+++ YogaFrontend/src/components/Header.jsx
@@ -3,8 +3,11 @@ import NavBar from './NavBar'
 export default function Header() {
     return (
         <header>
-            <h1>Marcela Christjansen Yoga</h1>
-            <button>Sign up</button>
+            <div className="header-div">
+                <h1> </h1>
+                <h1>Marcela Christjansen Yoga</h1>
+                <button className="signUpButton">Sign up</button>
+            </div>
             <NavBar />
         </header>
     )
