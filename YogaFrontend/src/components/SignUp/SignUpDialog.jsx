7b4cf315f7e9cf9096219d0d7fb79e6bd464d26e
TheDanishMexican <danieljchristjansen@gmail.com>
2023-12-04 11:11:21 +0100

    -

--- YogaFrontend/src/components/SignUp/SignUpDialog.jsx
+++ YogaFrontend/src/components/SignUp/SignUpDialog.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import AnimatedPage from '../AnimatedPage'
 
 export default function SignUpDialog({ handleCloseDialog }) {
     SignUpDialog.propTypes
@@ -47,85 +48,87 @@ export default function SignUpDialog({ handleCloseDialog }) {
     }
 
     return (
-        <div className="dialog">
-            <dialog className="signup-dialog" open>
-                <button
-                    onClick={handleCloseDialog}
-                    className="close-dialog-button"
-                >
-                    X
-                </button>
-                <h2>Sign up</h2>
-                <form onSubmit={handleSubmit}>
-                    <label htmlFor="age">Age:</label>
-                    <input
-                        type="number"
-                        id="age"
-                        name="age"
-                        value={formData.age}
-                        onChange={handleChange}
-                        required
-                    />
-
-                    <label htmlFor="firstName">First Name:</label>
-                    <input
-                        type="text"
-                        id="firstName"
-                        name="firstName"
-                        value={formData.firstName}
-                        onChange={handleChange}
-                        required
-                    />
-
-                    <label htmlFor="lastName">Last Name:</label>
-                    <input
-                        type="text"
-                        id="lastName"
-                        name="lastName"
-                        value={formData.lastName}
-                        onChange={handleChange}
-                        required
-                    />
-
-                    <label htmlFor="activityLevel">Activity Level:</label>
-                    <br />
-                    <select
-                        id="activityLevel"
-                        name="activityLevel"
-                        value={formData.activityLevel}
-                        onChange={handleChange}
-                        required
+        <AnimatedPage>
+            <div className="dialog">
+                <dialog className="signup-dialog" open>
+                    <button
+                        onClick={handleCloseDialog}
+                        className="close-dialog-button"
                     >
-                        <option value="Low">Low</option>
-                        <option value="Medium">Medium</option>
-                        <option value="High">High</option>
-                    </select>
-                    <br />
-                    <label htmlFor="phone">Phone Number:</label>
-                    <input
-                        type="tel"
-                        id="phone"
-                        name="phone"
-                        pattern="[0-9]{10}"
-                        placeholder="Enter a 10-digit phone number without spaces or dashes."
-                        value={formData.phone}
-                        onChange={handleChange}
-                        required
-                    />
+                        X
+                    </button>
+                    <h2>Sign up</h2>
+                    <form onSubmit={handleSubmit}>
+                        <label htmlFor="age">Age:</label>
+                        <input
+                            type="number"
+                            id="age"
+                            name="age"
+                            value={formData.age}
+                            onChange={handleChange}
+                            required
+                        />
 
-                    <label htmlFor="email">Email:</label>
-                    <input
-                        type="email"
-                        id="email"
-                        name="email"
-                        value={formData.email}
-                        onChange={handleChange}
-                        required
-                    />
+                        <label htmlFor="firstName">First Name:</label>
+                        <input
+                            type="text"
+                            id="firstName"
+                            name="firstName"
+                            value={formData.firstName}
+                            onChange={handleChange}
+                            required
+                        />
 
-                    <button type="submit">Submit</button>
-                </form>
-            </dialog>
-        </div>
+                        <label htmlFor="lastName">Last Name:</label>
+                        <input
+                            type="text"
+                            id="lastName"
+                            name="lastName"
+                            value={formData.lastName}
+                            onChange={handleChange}
+                            required
+                        />
+
+                        <label htmlFor="activityLevel">Activity Level:</label>
+                        <br />
+                        <select
+                            id="activityLevel"
+                            name="activityLevel"
+                            value={formData.activityLevel}
+                            onChange={handleChange}
+                            required
+                        >
+                            <option value="Low">Low</option>
+                            <option value="Medium">Medium</option>
+                            <option value="High">High</option>
+                        </select>
+                        <br />
+                        <label htmlFor="phone">Phone Number:</label>
+                        <input
+                            type="tel"
+                            id="phone"
+                            name="phone"
+                            pattern="[0-9]{10}"
+                            placeholder="Enter a 10-digit phone number without spaces or dashes."
+                            value={formData.phone}
+                            onChange={handleChange}
+                            required
+                        />
+
+                        <label htmlFor="email">Email:</label>
+                        <input
+                            type="email"
+                            id="email"
+                            name="email"
+                            value={formData.email}
+                            onChange={handleChange}
+                            required
+                        />
+
+                        <button type="submit">Submit</button>
+                    </form>
+                </dialog>
+            </div>
+        </AnimatedPage>
     )
 }
