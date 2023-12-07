427ba01af57e759136ddf3c2b95ac14c5752c4b8
TheDanishMexican <danieljchristjansen@gmail.com>
2023-12-07 12:25:35 +0100

    event signups overview works

--- YogaFrontend/src/components/Adminpage/Event overview/EventSignups.jsx
+++ YogaFrontend/src/components/Adminpage/Event overview/EventSignups.jsx
@@ -24,7 +24,35 @@ export default function EventSignups() {
         try {
             const response = await fetch('http://localhost:3000/events')
             const data = await response.json()
-            setEventData(data)
+            const sortedEvents = data.sort(
+                (a, b) => new Date(b.start) - new Date(a.start)
+            )
+            const formattedEvents = sortedEvents.map((event) => {
+                const formattedStartDate = new Date(event.start).toDateString()
+                const formattedEndDate = new Date(event.end).toDateString()
+                const formattedStartTime = new Date(
+                    event.start
+                ).toLocaleTimeString([], {
+                    hour: '2-digit',
+                    minute: '2-digit',
+                })
+                const formattedEndTime = new Date(event.end).toLocaleTimeString(
+                    [],
+                    {
+                        hour: '2-digit',
+                        minute: '2-digit',
+                    }
+                )
+
+                return {
+                    ...event,
+                    start: formattedStartDate,
+                    end: formattedEndDate,
+                    startTime: formattedStartTime,
+                    endTime: formattedEndTime,
+                }
+            })
+            setEventData(formattedEvents)
         } catch (error) {
             console.log('Error fetching events:', error)
         }
