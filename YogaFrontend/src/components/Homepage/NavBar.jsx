ce1d311438a961cadc0bdb27e0afdbd6fd63ea7b
TheDanishMexican <danieljchristjansen@gmail.com>
2023-12-08 13:56:43 +0100

    .

--- YogaFrontend/src/components/Homepage/NavBar.jsx
+++ YogaFrontend/src/components/Homepage/NavBar.jsx
@@ -6,7 +6,7 @@ export default function NavBar() {
             <ul className="nav-items">
                 <NavLink to="/">home</NavLink>
                 <NavLink to="/media">how I help you</NavLink>
-                <NavLink to="/events">ask me a question</NavLink>
+                <NavLink to="/events">FAQ</NavLink>
                 <NavLink to="/admin">contact</NavLink>
             </ul>
         </nav>
