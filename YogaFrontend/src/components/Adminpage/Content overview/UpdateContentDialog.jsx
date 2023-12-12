b3c7064491fc152aa7068d55e9db096128120b96
TheDanishMexican <danieljchristjansen@gmail.com>
2023-12-12 16:19:34 +0100

    added animations to add content and edit content dialogs

--- YogaFrontend/src/components/Adminpage/Content overview/UpdateContentDialog.jsx
+++ YogaFrontend/src/components/Adminpage/Content overview/UpdateContentDialog.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-
+import AnimatedPage from '../../Homepage/AnimatedPage'
 export default function UpdateFormDialog({ post, onClose, onUpdate }) {
     UpdateFormDialog.propTypes
     // State to store updated data with initial values from the post
@@ -47,79 +47,81 @@ export default function UpdateFormDialog({ post, onClose, onUpdate }) {
 
     return (
         <>
-            <div>
-                <dialog className="update-form-dialog" open>
-                    <h2 className="edit-content-title">Edit Post</h2>
+            <AnimatedPage>
+                <div>
+                    <dialog className="update-form-dialog" open>
+                        <h2 className="edit-content-title">Edit</h2>
 
-                    <form onSubmit={handleUpdate}>
-                        <div className="edit-content-container">
-                            <div className="edit-content-row">
-                                <div className="edit-content-input-container">
-                                    <label>Title:</label>
-                                    <input
-                                        type="text"
-                                        name="title"
-                                        value={updatedData.title}
-                                        onChange={handleInputChange}
-                                    />
+                        <form onSubmit={handleUpdate}>
+                            <div className="edit-content-container">
+                                <div className="edit-content-row">
+                                    <div className="edit-content-input-container">
+                                        <label>Title:</label>
+                                        <input
+                                            type="text"
+                                            name="title"
+                                            value={updatedData.title}
+                                            onChange={handleInputChange}
+                                        />
+                                    </div>
+                                    <div className="edit-content-input-container">
+                                        <label>Link:</label>
+                                        <input
+                                            type="text"
+                                            name="link"
+                                            value={updatedData.link}
+                                            onChange={handleInputChange}
+                                        />
+                                    </div>
+                                    <div className="edit-content-input-container">
+                                        <label>Link Description:</label>
+                                        <input
+                                            type="text"
+                                            name="linkDescription"
+                                            value={updatedData.linkDescription}
+                                            onChange={handleInputChange}
+                                        />
+                                    </div>
                                 </div>
-                                <div className="edit-content-input-container">
-                                    <label>Link:</label>
-                                    <input
-                                        type="text"
-                                        name="link"
-                                        value={updatedData.link}
-                                        onChange={handleInputChange}
-                                    />
-                                </div>
-                                <div className="edit-content-input-container">
-                                    <label>Link Description:</label>
-                                    <input
-                                        type="text"
-                                        name="linkDescription"
-                                        value={updatedData.linkDescription}
-                                        onChange={handleInputChange}
-                                    />
+                                <div className="edit-content-row">
+                                    <div className="edit-content-input-container">
+                                        <label>Description:</label>
+                                        <textarea
+                                            className="edit-content-textarea"
+                                            rows="5"
+                                            cols="23"
+                                            name="description"
+                                            value={updatedData.description}
+                                            onChange={handleInputChange}
+                                        />
+                                    </div>
+                                    <div className="edit-content-input-container">
+                                        <label>Image URL:</label>
+                                        <input
+                                            type="text"
+                                            name="img"
+                                            value={updatedData.img}
+                                            onChange={handleInputChange}
+                                        />
+                                    </div>
                                 </div>
                             </div>
-                            <div className="edit-content-row">
-                                <div className="edit-content-input-container">
-                                    <label>Description:</label>
-                                    <textarea
-                                        className="edit-content-textarea"
-                                        rows="5"
-                                        cols="23"
-                                        name="description"
-                                        value={updatedData.description}
-                                        onChange={handleInputChange}
-                                    />
-                                </div>
-                                <div className="edit-content-input-container">
-                                    <label>Image URL:</label>
-                                    <input
-                                        type="text"
-                                        name="img"
-                                        value={updatedData.img}
-                                        onChange={handleInputChange}
-                                    />
-                                </div>
+                            <div className="edit-buttons-container">
+                                <button className="logout-button" type="submit">
+                                    Update Post
+                                </button>
+                                <button
+                                    className="logout-button"
+                                    type="button"
+                                    onClick={onClose}
+                                >
+                                    Cancel
+                                </button>
                             </div>
-                        </div>
-                        <div className="edit-buttons-container">
-                            <button className="logout-button" type="submit">
-                                Update Post
-                            </button>
-                            <button
-                                className="logout-button"
-                                type="button"
-                                onClick={onClose}
-                            >
-                                Cancel
-                            </button>
-                        </div>
-                    </form>
-                </dialog>
-            </div>
+                        </form>
+                    </dialog>
+                </div>
+            </AnimatedPage>
         </>
     )
 }
